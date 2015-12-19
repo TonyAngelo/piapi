d52d7ab8cfa3aa82bcf7f07897b0803f1e61ef55
Tony Petrangelo <tonypetrangelo@gmail.com>
2015-12-19 13:06:39 -0600

    added a route

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 # API app for controlling AV devices
 
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Petraberg AV Control Server'
+	i={
+		'name':'Petraberg AV System',
+		'location':'Minneapolis, MN'
+	}
+    return json.dumps(i)
+
+@app.route('/tv')
+def index():
+	i={
+		'name':'Control TV',
+		'action':'Some Thing'
+	}
+    return json.dumps(i)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
