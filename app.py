1d3938699f194035a847e005a2d91a5751ea2be8
Tony Petrangelo <tonypetrangelo@gmail.com>
2015-12-24 10:55:00 -0600

    fixed jsonify and response

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # API app for controlling AV devices
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
 #import user
-#import json
+import json
 import directv
 
 dtv = directv.DirecTV("192.168.88.253")
@@ -18,35 +18,37 @@ def check_key(key):
 def index():
 	key_check = check_key(request.args.get('key'))
 	if key_check:
-                return jsonify({'name':'Petraberg AV System','location':'Minneapolis, MN'})
+                return jsonify(name='Petraberg AV System',location='Minneapolis, MN')
+                #return "Valid Key" + request
         else:
-                return jsonify({'error':'Invalid Key'})
+                return jsonify(error='Invalid Key')
+                #return "Invalid Key"
 
 @app.route('/dtv/key/<key>', methods=['GET'])
 def dtv_key(key):
 	key_check = check_key(request.args.get('key'))
 	if key_check:
 		dtv.sendkey(key,'keyPress')
-                return jsonify({'keyPress':key})
+                return jsonify(keyPress=key)
 	else:
-                return jsonify({'error':'Invalid Key'})
+                return jsonify(error='Invalid Key')
 
 @app.route('/dtv/chan/<chan>', methods=['GET'])
 def dtv_chan(chan):
 	key_check = check_key(request.args.get('key'))
 	if key_check:
 		dtv.sendchan(chan,'0')
-                return jsonify({'channel':chan})
+                return jsonify(channel=chan)
 	else:
-                return jsonify({'error':'Invalid Key'})
+                return jsonify(error='Invalid Key')
 
 @app.errorhandler(404)
 def not_found(error):
 	key_check = check_key(request.args.get('key'))
 	if key_check:
-                return make_response(jsonify({'error': 'Not found'}), 404)
+                return make_response(jsonify(error='Not found'), 404)
         else:
-                return jsonify({'error':'Invalid Key'})
+                return jsonify(error='Invalid Key')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
