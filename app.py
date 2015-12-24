94593c3205633d2e8fe4bfbe63eb925d8089c736
Tony Petrangelo <tonypetrangelo@gmail.com>
2015-12-24 10:03:57 -0600

    added basic key

--- app.py
+++ app.py
@@ -1,27 +1,52 @@
 # API app for controlling AV devices
 
-from flask import Flask
-import json
+from flask import Flask, make_response
+#import user
+#import json
 import directv
 
 dtv = DirecTV("192.168.88.253")
 app = Flask(__name__)
 
-@app.route('/')
+def check_key(key):
+	if key=='SOMESORTOFFNORDKEYHASH':
+		return True
+	else:
+		return False
+
+@app.route('/', methods=['GET'])
 def index():
-	i={
-		'name':'Petraberg AV System',
-		'location':'Minneapolis, MN'
-	}
-    return json.dumps(i)
+	key_check = check_key(request.args.get('key'))
+	if key_check:
+    	return jsonify({'name':'Petraberg AV System','location':'Minneapolis, MN'})
+    else:
+    	return jsonify({'error':'Invalid Key'})
 
-@app.route('/dtv/key/<key>')
+@app.route('/dtv/key/<key>', methods=['GET'])
 def dtv_key(key):
-	dtv.sendkey(key,'keyPress')
+	key_check = check_key(request.args.get('key'))
+	if key_check:
+		dtv.sendkey(key,'keyPress')
+	    return jsonify({'keyPress':key})
+	else:
+    	return jsonify({'error':'Invalid Key'})
 
-@app.route('/dtv/chan/<chan>')
+@app.route('/dtv/chan/<chan>', methods=['GET'])
 def dtv_key(chan):
-	dtv.sendchan(chan,0)
+	key_check = check_key(request.args.get('key'))
+	if key_check:
+		dtv.sendchan(chan,'0')
+	    return jsonify({'channel':chan})
+	else:
+    	return jsonify({'error':'Invalid Key'})
+
+@app.errorhandler(404)
+def not_found(error):
+	key_check = check_key(request.args.get('key'))
+	if key_check:
+    	return make_response(jsonify({'error': 'Not found'}), 404)
+    else:
+    	return jsonify({'error':'Invalid Key'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
