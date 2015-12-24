99559c5dbd72201a9807b1ea9914b0317a3468c5
Tony Petrangelo <tonypetrangelo@gmail.com>
2015-12-24 10:27:14 -0600

    misc updates

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from flask import Flask, make_response
 #import json
 import directv
 
-dtv = DirecTV("192.168.88.253")
+dtv = directv.DirecTV("192.168.88.253")
 app = Flask(__name__)
 
 def check_key(key):
-	if key=='SOMESORTOFFNORDKEYHASH':
+        if key=='SOMESORTOFFNORDKEYHASH':
 		return True
 	else:
 		return False
@@ -18,35 +18,35 @@ def check_key(key):
 def index():
 	key_check = check_key(request.args.get('key'))
 	if key_check:
-    	return jsonify({'name':'Petraberg AV System','location':'Minneapolis, MN'})
-    else:
-    	return jsonify({'error':'Invalid Key'})
+                return jsonify({'name':'Petraberg AV System','location':'Minneapolis, MN'})
+        else:
+                return jsonify({'error':'Invalid Key'})
 
 @app.route('/dtv/key/<key>', methods=['GET'])
 def dtv_key(key):
 	key_check = check_key(request.args.get('key'))
 	if key_check:
 		dtv.sendkey(key,'keyPress')
-	    return jsonify({'keyPress':key})
+                return jsonify({'keyPress':key})
 	else:
-    	return jsonify({'error':'Invalid Key'})
+                return jsonify({'error':'Invalid Key'})
 
 @app.route('/dtv/chan/<chan>', methods=['GET'])
-def dtv_key(chan):
+def dtv_chan(chan):
 	key_check = check_key(request.args.get('key'))
 	if key_check:
 		dtv.sendchan(chan,'0')
-	    return jsonify({'channel':chan})
+                return jsonify({'channel':chan})
 	else:
-    	return jsonify({'error':'Invalid Key'})
+                return jsonify({'error':'Invalid Key'})
 
 @app.errorhandler(404)
 def not_found(error):
 	key_check = check_key(request.args.get('key'))
 	if key_check:
-    	return make_response(jsonify({'error': 'Not found'}), 404)
-    else:
-    	return jsonify({'error':'Invalid Key'})
+                return make_response(jsonify({'error': 'Not found'}), 404)
+        else:
+                return jsonify({'error':'Invalid Key'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
